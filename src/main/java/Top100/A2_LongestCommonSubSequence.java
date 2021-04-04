ae9da34a2a766753664aaa4007f9bd6000f9956e
“Akshay <aksinghai@outlook.com>
2021-04-04 22:59:57 +0530

    A2_LongestCommonSubSequence Tree Approach

--- src/main/java/Top100/A2_LongestCommonSubSequence.java
+++ src/main/java/Top100/A2_LongestCommonSubSequence.java
@@ -1,7 +1,30 @@
 package Top100;
 
+import java.util.Map;
+
 public class A2_LongestCommonSubSequence {
     public static void main(String[] args) {
+        String s1 = "ABCBDAB";
+        String s2 = "BDCABA";
+        solution1(s1, s2, s1.length()-1, s2.length()-1, "");
+    }
+
+    //Tree Approach
+    private static void solution1(String s1, String s2, int n1, int n2, String output){
+        if(n1 < 0 || n2 < 0){
+            System.out.println(output);
+            return;
+        }
+        if(s1.charAt(n1) == s2.charAt(n2)){
+            solution1(s1,s2,n1 - 1, n2 - 1, s1.charAt(n1) + output);
+        } else {
+            solution1(s1, s2, n1 - 1, n2, output);
+            solution1(s1, s2, n1, n2 - 1, output);
+        }
+    }
+
+    //Optimized Approach
+    private static void solution2(String s1, String s2){
 
     }
 }
